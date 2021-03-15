70d54c260cb40c302cdd6db2539c64abe151696b
Tihici <tihamer.sebok@gmail.com>
2021-03-15 16:14:02 +0200

    v-bind, v-model, v-on data.methods -osztalynevsor

--- scripts.js
+++ scripts.js
@@ -2,6 +2,33 @@ let app = new Vue({
     el: 'main#app',
     data: {
         pageTitle: 'Osztalynaplo',
-        studentCollection: ["Anna","Bela","Ferenc","Julia","Joska"]
+        studentCollection: [
+            {name: "Anna", gender: "lany"},
+            {name: "Joska", gender: "fiu"},
+            {name: "Panna", gender: "lany"},
+            {name: "Istvan", gender: "fiu"},
+            {name: "Olga", gender: "lany"}
+        ],
+        newStudent: {
+            name: '',
+            gender: ''
+        }
+    },
+    methods: {
+        AddNewStudent() {
+            if (!this.newStudent) {
+                return;
+            }
+            this.studentCollection.push(this.newStudent);
+            this.newStudent = {name: '', gender: ''};
+        },
+        GetColor: function (student) {
+            let backgroundColor = student.gender === 'fiu' ? 'blue' : 'red';
+            return {
+                color: 'white',
+                backgroundColor: backgroundColor
+
+            }
+        }
     }
 });
